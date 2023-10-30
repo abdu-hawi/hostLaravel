a2d55cf5615e7accbdf1c2f4a10fe1b9d3dcc8ad
abdu-hawi <ahhh42@gmail.com>
2023-10-30 23:25:26 +0300

    contact

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,7 @@ Route::get('qr_email', function () {
         ->gradient(48, 48, 49, 99, 99, 197, "diagonal")
         ->backgroundColor(246, 248, 250)
         ->generate("https://techvblogs.com/blog/generate-qr-code-laravel-9");
+    print_r($qr);
     return gettype($qr);
     Mail::to('ahhh42@gmail.com')->send(new SendEmailRigester([
         "name" => "Abdu Hawi",
