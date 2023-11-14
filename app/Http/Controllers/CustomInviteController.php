3129b865e0c24a31b2602c7d7fdd5bc6afb78369
abdu-hawi <ahhh42@gmail.com>
2023-11-14 13:13:06 +0300

    google_tag

--- app/Http/Controllers/CustomInviteController.php
+++ app/Http/Controllers/CustomInviteController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Mail\SendEmailRigester;
 use App\Models\Client;
 use App\Models\EmailFailer;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class CustomInviteController extends Controller
 {
@@ -21,30 +21,29 @@ class CustomInviteController extends Controller
     protected function save(Request $request)
     {
         $data = $request->validate([
-            "first_name" => "required | string",
-            "last_name" => "required | string",
-            "job_title" => "required | string",
-            "company_name" => "required | string",
-            "mobile" => "required | regex:/^([0-9\s\-\+\(\)]*)$/",
-            "email" => "required | email | unique:clients,email",
-            "industry" => "required | string",
-            "interested" => "required | string",
+            'first_name' => 'required | string',
+            'last_name' => 'required | string',
+            'job_title' => 'required | string',
+            'company_name' => 'required | string',
+            'mobile' => "required | regex:/^([0-9\s\-\+\(\)]*)$/",
+            'email' => 'required | email | unique:clients,email',
+            'industry' => 'required | string',
+            'interested' => 'required | string',
         ]);
 
         try {
             $client = Client::query()->create($data);
-            $url = route('presence',[
+            $url = route('presence', [
                 'uuid' => \Str::uuid(),
-                'id' => $client->id
+                'id' => $client->id,
             ]);
 
-            $vip = 'VIP SCES Invitation';
-                $qr = QrCode::size(300)
-                ->format('png')
-                ->gradient(49, 49, 48, 192, 136, 2, 'diagonal')
-                ->backgroundColor(246, 248, 250)
-                ->generate($url);
-
+            $vip = 'SCES VIP Invitation';
+            $qr = QrCode::size(300)
+            ->format('png')
+            ->gradient(49, 49, 48, 192, 136, 2, 'diagonal')
+            ->backgroundColor(246, 248, 250)
+            ->generate($url);
 
             $output_file = \Str::uuid().time().'.png';
             Storage::disk('public')->put('qr/'.$output_file, $qr);
@@ -59,29 +58,29 @@ class CustomInviteController extends Controller
                 'qr' => $qr,
                 'isEmail' => false,
                 'vip' => $vip,
-                'cc' => 'cc'
+                'cc' => 'cc',
             ]))) {
                 $client->query()->update([
-                    'is_sent_email' => true
+                    'is_sent_email' => true,
                 ]);
             } else {
                 EmailFailer::query()->create([
                     'email' => $data['email'],
-                    'fails' => 'Unkown Error'
+                    'fails' => 'Unkown Error',
                 ]);
             }
 
             if (Storage::disk('public')->exists('qr/'.$output_file)) {
                 Storage::disk('public')->delete('qr/'.$output_file);
             }
-
         } catch (\Exception $ex) {
             EmailFailer::query()->create([
                 'email' => $data['email'],
-                'fails' => $ex->getMessage()
+                'fails' => $ex->getMessage(),
             ]);
         }
         Session::flash('success', 'The invite send successfully');
+
         return redirect(route('register'));
     }
 }
