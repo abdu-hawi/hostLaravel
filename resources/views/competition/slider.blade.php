e476411ea881f56a229800dbf514fad02e5b2483
abdu-hawi <ahhh42@gmail.com>
2023-11-15 10:35:20 +0300

    competition

--- resources/views/competition/slider.blade.php
+++ resources/views/competition/slider.blade.php
@@ -17,25 +17,42 @@
                         class="elementor-container elementor-column-gap-default">
                         <div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-3604778"
                             data-id="3604778" data-element_type="column">
+                            <style>
+                                .ah-h2{
+                                    font-size: 58px !important;
+                                }
+                                .ah-h4{
+                                    font-size: 20px !important;
+                                }
+                                @media(max-width:767px){
+                                    .ah-h2{
+                                        font-size: 40px !important;
+                                    }
+                                    .ah-h4{
+                                        font-size: 14px !important;
+                                    }
+                                }
+                            </style>
                             <div
                                 class="elementor-widget-wrap elementor-element-populated">
                                 <div class="elementor-element elementor-element-46fae01 elementor-widget elementor-widget-heading"
                                     data-id="46fae01" data-element_type="widget"
                                     data-widget_type="heading.default">
                                     <div class="elementor-widget-container">
-                                        <h2 class="elementor-heading-title elementor-size-default" style="font-size: 58px;">
+                                        <h2 class="elementor-heading-title elementor-size-default ah-h2">
                                             {{ __('competition.PITCH_COMPETITION') }}
                                         </h2>
                                     </div>
                                 </div>
+
                                 <div class="elementor-element elementor-element-2891c96 elementor-widget elementor-widget-heading"
                                     data-id="2891c96" data-element_type="widget"
                                     data-widget_type="heading.default">
                                     <div class="elementor-widget-container" style="margin-top: 2em;">
-                                        <h4 class="elementor-heading-title elementor-size-default" style="font-size: 20px;">
+                                        <h4 class="elementor-heading-title elementor-size-default ah-h4">
                                             {{ __('competition.are_you_an_innovative_entrepreneur') }}
                                         </h4>
-                                        <h4 class="elementor-heading-title elementor-size-default color-f0f9ff mt-1" style="font-size: 20px;">
+                                        <h4 class="elementor-heading-title elementor-size-default color-f0f9ff mt-1 ah-h4">
                                             {{ __('competition.apply_today_to_the_pitch_competition') }}
                                         </h4>
                                     </div>
