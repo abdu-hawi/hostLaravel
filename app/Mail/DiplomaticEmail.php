e42d7a62415d17d948c53b8255323bb8c74b811c
abdu-hawi <ahhh42@gmail.com>
2023-11-16 18:30:02 +0300

    agenda

--- app/Mail/DiplomaticEmail.php
+++ app/Mail/DiplomaticEmail.php
@@ -27,6 +27,7 @@ class DiplomaticEmail extends Mailable
             ->view('emails.diplomatic', [
                 'data' => $this->data,
                 'contact_us' => route('contact_us'),
+                'agenda' => route('agenda'),
                 'xs_logo' => asset('wp-content/uploads/2023/01/xs-conference-1-light.png'),
                 'c3_logo' => asset('wp-content/uploads/2023/01/C3-conference-1-e1675154716886-150x150.jpg'),
                 'sce_summit_logo' => asset('wp-content/uploads/2023/07/logo_black.jpg'),
--- app/Mail/SendEmailRigester.php
+++ app/Mail/SendEmailRigester.php
@@ -28,6 +28,7 @@ class SendEmailRigester extends Mailable
             ->view('emails.update_date', [
                 'data' => $this->data,
                 'contact_us' => route('contact_us'),
+                'agenda' => route('agenda'),
                 'xs_logo' => asset('wp-content/uploads/2023/01/xs-conference-1-light.png'),
                 'c3_logo' => asset('wp-content/uploads/2023/01/C3-conference-1-e1675154716886-150x150.jpg'),
                 'sce_summit_logo' => asset('wp-content/uploads/2023/07/logo_black.jpg'),
@@ -43,6 +44,7 @@ class SendEmailRigester extends Mailable
             ->view('emails.sample', [
                 'data' => $this->data,
                 'contact_us' => route('contact_us'),
+                'agenda' => route('agenda'),
                 'xs_logo' => asset('wp-content/uploads/2023/01/xs-conference-1-light.png'),
                 'c3_logo' => asset('wp-content/uploads/2023/01/C3-conference-1-e1675154716886-150x150.jpg'),
                 'sce_summit_logo' => asset('wp-content/uploads/2023/07/logo_black.jpg'),
